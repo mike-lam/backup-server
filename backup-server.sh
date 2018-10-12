61e8275586f5f5c4314d39261017c219add6fb0e
Ubuntu <vmadmin@ubuntu-gitlabstack05.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-10-12 20:03:42 +0000

    pretty comments

--- backup-server.sh
+++ backup-server.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 #set -x
-#echo "DEBUG 1">> /var/log/vsftpd/debug.log
-#while true; do  #loop infinitely to produce backups or delete old backups every $SLEEP time
-#  sleep 9999
-#  echo "delete the loop DEBUG" >>>> /var/log/vsftpd/debug.log
-#done
 
-# these GLOBAL variables should be set in docker-compose.yml file as environment variables, however default values are provided here which makes testing easier to do.
-TMPDIR=${TMPDIR:-/tmp}
-#FTP_SERVER=${FTP_SERVER:-ubuntu-gitlabstack05}
-#FTP_USER=${FTP_USER:-vmadmin}
-#FTP_PASSWD=${FTP_PASSWD:-Dc5k20a3}
-#SLEEP_INIT=${SLEEP_INIT:-1s}
+# these GLOBAL variables must be set in docker-compose.yml file as environment variables.
 #SLEEP=${SLEEP:-10m}
 #DELETE_MTIME=${DELETE_MTIME:-5}
 #DELETE_LOG_SIZE=${DELETE_LOG_SIZE:-10}
 #STACK_NAMESPACE
-BACKUPDIR=""
 
 #these GLOBAL variables are calculated
 
@@ -30,7 +19,6 @@ delete_old_backups() {
   tail -n $DELETE_LOG_SIZE  /var/log/vsftpd/backup-server.log >  /var/log/vsftpd/backup-server.log
 }
 
-sleep 1s #$SLEEP_INIT  #give other container some lead time to start running
 while true; do  #loop infinitely to delete old backups every $SLEEP time
   delete_old_backups
   sleep $SLEEP
