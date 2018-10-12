0d37f10dc7285f978684adb168871e8d6f8f5d63
Ubuntu <vmadmin@ubuntu-gitlabstack05.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-10-12 19:58:35 +0000

    tested to work without cmd

--- backup-server.sh
+++ backup-server.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 #set -x
-echo "DEBUG 1">> /var/log/vsftpd/debug.log
-while true; do  #loop infinitely to produce backups or delete old backups every $SLEEP time
-  sleep 9999
-  echo "delete the loop DEBUG" >>>> /var/log/vsftpd/debug.log
-done
+#echo "DEBUG 1">> /var/log/vsftpd/debug.log
+#while true; do  #loop infinitely to produce backups or delete old backups every $SLEEP time
+#  sleep 9999
+#  echo "delete the loop DEBUG" >>>> /var/log/vsftpd/debug.log
+#done
 
 # these GLOBAL variables should be set in docker-compose.yml file as environment variables, however default values are provided here which makes testing easier to do.
 TMPDIR=${TMPDIR:-/tmp}
@@ -14,39 +15,24 @@ TMPDIR=${TMPDIR:-/tmp}
 #SLEEP=${SLEEP:-10m}
 #DELETE_MTIME=${DELETE_MTIME:-5}
 #DELETE_LOG_SIZE=${DELETE_LOG_SIZE:-10}
+#STACK_NAMESPACE
 BACKUPDIR=""
 
 #these GLOBAL variables are calculated
-setNODE_IP() {
-  NODE_IP=$(docker info --format '{{.Swarm.NodeAddr}}')
-}
-setNODE_IP
-
-setSTACK_NAMESPACE() {
-  STACK_NAMESPACE=$(docker inspect --format '{{index .Config.Labels "com.docker.stack.namespace"}}' $(hostname) 2> /dev/null)
-  if [ "$?" != "0" ]; then
-    STACK_NAMESPACE="gitlabstack" #for testing outside containers 
-  fi
-}
-setSTACK_NAMESPACE
 
 #--------------------
 delete_old_backups() {
   #delete backup dirs older then $DELETE_MTIME, also keep only last $DELETE_LOG_SIZE lines of delete logs
-  echo "delete_old_backups"
-#  echo "Started delete_old_backups on $(hostname) at $(date)"  2>&1 | tee -a  $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
-#  find $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER -mtime +$DELETE_MTIME  2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
-#  find $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER -mtime +$DELETE_MTIME -exec rm -r {} \;
-#  echo "DONE with delete_old_backups at $(date)!"  2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
-#  tail -n $DELETE_LOG_SIZE  $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log >  $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/cron.log
-#  rm $DOCKER_ROOT_DIR/volumes/"$STACK_NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
+  echo "Started delete_old_backups for stack $STACK_NAMESPACE at $(date)"  2>&1 | tee -a  /var/log/vsftpd/backup-server.log
+  find /home/vsftpd/vmadmin -mtime +$DELETE_MTIME   2>&1 | tee -a  /var/log/vsftpd/backup-server.log
+  find /home/vsftpd/vmadmin -mtime +$DELETE_MTIME -exec rm -r {} \;
+  echo "DONE with delete_old_backups at $(date)!"  2>&1 | tee -a /var/log/vsftpd/backup-server.log
+  tail -n $DELETE_LOG_SIZE  /var/log/vsftpd/backup-server.log >  /var/log/vsftpd/backup-server.log
 }
 
 sleep 1s #$SLEEP_INIT  #give other container some lead time to start running
 while true; do  #loop infinitely to delete old backups every $SLEEP time
-  if [ "$NODE_IP" == "$FTP_SERVER" ]; then
-   delete_old_backups
-  fi
+  delete_old_backups
   sleep $SLEEP
 done
 
