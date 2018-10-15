569b11e209e7266c4fb049c420e44f3b2d7748a3
Ubuntu <vmadmin@ubuntu-gitlabstack05.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-10-15 19:09:57 +0000

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # backup-server
+
+in the docker-compose-yml use
+
+```yaml
+  backup_server:
+    image: 'mxl125/backup-server:latest'
+    deploy:
+      placement:
+        constraints:
+          - node.hostname == some fix node to keep all backups together on a separate vm if this changes then passv_address must change 05=146
+    environment:
+      - FTP_USER=ftp userid
+      - FTP_PASS=userid password
+      - PASV_ADDRESS=ip of node where server is deployed
+      - PASV_PROMISCUOUS=YES
+      - PASV_MIN_PORT=21100
+      - PASV_MAX_PORT=21110
+      - SLEEP how long to sleep before deleting old backups (i.e. 10m)
+      - DELETE_MTIME how old the backups needs to be before we delete them (i.e. 5)
+      - LOG_SIZE   nbr of lines to keep in the log (i.e. 10)
+      - STACK_NAMESPACE={{index .Service.Labels "com.docker.stack.namespace"}}
+    hostname: '{{.Service.Name}}'
+    ports:
+      - '21:21'
+      - '20:20'
+      - '21100-21110:21100-21110'
+    volumes:
+      - 'backup_server:/home/vsftpd'
+      - 'backup_server-log:/var/log/vsftpd'
+
+  backup_client:
+    see the mxl125/backup-client image doc
+```
+
+see also the FROM image fauria/vsftpd 
+
--- backup-server.sh
+++ backup-server.sh
@@ -2,25 +2,28 @@
 #set -x
 
 # these GLOBAL variables must be set in docker-compose.yml file as environment variables.
-#SLEEP=${SLEEP:-10m}
-#DELETE_MTIME=${DELETE_MTIME:-5}
-#DELETE_LOG_SIZE=${DELETE_LOG_SIZE:-10}
-#STACK_NAMESPACE
+#SLEEP how long to sleep before deleting old backups
+#DELETE_MTIME how old the backups needs to be before we delete them
+#LOG_SIZE   nbr of lines to keep in the log
+#STACK_NAMESPACE 
 
-#these GLOBAL variables are calculated
-
-#--------------------
 delete_old_backups() {
-  #delete backup dirs older then $DELETE_MTIME, also keep only last $DELETE_LOG_SIZE lines of delete logs
+  #delete backup dirs older then $DELETE_MTIME
   echo "Started delete_old_backups for stack $STACK_NAMESPACE at $(date)"  2>&1 | tee -a  /var/log/vsftpd/backup-server.log
   find /home/vsftpd/vmadmin -mtime +$DELETE_MTIME   2>&1 | tee -a  /var/log/vsftpd/backup-server.log
   find /home/vsftpd/vmadmin -mtime +$DELETE_MTIME -exec rm -r {} \;
   echo "DONE with delete_old_backups at $(date)!"  2>&1 | tee -a /var/log/vsftpd/backup-server.log
-  tail -n $DELETE_LOG_SIZE  /var/log/vsftpd/backup-server.log >  /var/log/vsftpd/backup-server.log
+}
+
+keep_only_log_tail() {
+  cp /var/log/vsftpd/backup-server.log  /var/log/vsftpd/backup-server.tmp
+  tail -n $LOG_SIZE /var/log/vsftpd/backup-server.tmp > /var/log/vsftpd/backup-server.log
+  rm /var/log/vsftpd/backup-server.tmp
 }
 
 while true; do  #loop infinitely to delete old backups every $SLEEP time
   delete_old_backups
+  keep_only_log_tail
   sleep $SLEEP
 done
 
